cf3eb615625d243bdb99a7111e3f6b2167611249
GempyTon <Pendilgans@gmail.com>
2022-08-04 21:17:13 +0700

    V17

---
+++ config.js
@@ -0,0 +1,50 @@
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+//---WEBSITE APIKEY---//
+global.APIs = {
+	zenz: 'https://zenzapi.xyz',
+}
+
+// Apikey Website Api
+global.APIKeys = {
+	'https://zenzapi.xyz': 'ad9e14dd7c',
+}
+
+//---LAINNYA---///
+global.owner = ['6287898307350']
+global.packname = 'PENTABOT'
+global.author = '\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⭔'
+global.mess = {
+    success: '✓ Success',
+    admin: ' LU GBS PAKE FITUR KARNA CUMA BUAT ADMIN Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: 'Process...',
+}
+
+
+
+
+
+
+
+
+
+
+global.sessionName = 'GempyTon'    
+global.thumb = fs.readFileSync('./FotoMenu/Gempy.jpg')
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
